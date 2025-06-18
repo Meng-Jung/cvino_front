fd44eec7a4fe6a2b2cfc441a9c26ab0f4836151b
gaziza-jnb <gaziza.djn@gmail.com>
2025-06-18 18:29:38 +0500

    Delete app2.py

--- app2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Manual audio test")
-
-st.components.v1.html("""
-<audio controls>
-  <source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mp3">
-</audio>
-""", height=60)
